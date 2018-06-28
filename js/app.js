7c6ac1b789dbc073393f736fa126fa05024b4d1e
Sebastian Konicz <sebastian.konicz@gmail.com>
2018-06-28 17:29:34 +0200

    Zrobiony kalkulator ceny fotela

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log("------------Sterowanie Buttonami---------");
 document.addEventListener("DOMContentLoaded", function (){
     /**
      * Created by Jacek on 2016-01-12.
@@ -60,7 +59,6 @@ document.addEventListener("DOMContentLoaded", function (){
     }
 
     //PRICING HOVER
-
     var price = document.querySelectorAll(".pricing-banners-el");
 
     for (var i = 0; i < price.length; i++) {
@@ -97,5 +95,212 @@ document.addEventListener("DOMContentLoaded", function (){
         this.querySelector(".pricing-banner-btn").style.color = "#ffffff";
     }
 
+
+    //CALCULATOR
+    var arrowProduct = document.querySelector(".arrow-product");
+    var arrowColor = document.querySelector(".arrow-color");
+    var arrowPattern = document.querySelector(".arrow-pattern");
+
+    var panelLeft = document.querySelector(".panel_left");
+    var panelRight = document.querySelector(".panel_right");
+    var productPrice = panelRight.querySelector(".product");
+    var colorPrice = panelRight.querySelector(".color");
+    var patternPrice = panelRight.querySelector(".pattern");
+    var totalPrice = document.querySelector(".sum_total");
+
+
+    var productPrice = 0;
+    var colorPrice = 0;
+    var patternPrice = 0;
+    var transportPrice = 0;
+    var summaryPrice = 0;
+
+    function summary(){
+        summaryPrice = productPrice + colorPrice + patternPrice + transportPrice ;
+        totalPrice.innerText = summaryPrice;
+    }
+
+    arrowProduct.addEventListener("click", function (event) {
+        //Rozwijanie listy
+        var dropDown = this.parentElement;
+        var list = dropDown.querySelector(".list_panel");
+        list.classList.toggle("list_hide");
+
+        var choice = dropDown.querySelectorAll("li");
+        for (var i = 0; i < choice.length; i++) {
+
+            choice[i].addEventListener("click", function select(event) {
+
+                var dropDown = this.parentElement.parentElement;
+                var listLabel = dropDown.querySelector(".list_label");
+                listLabel.innerText = this.innerText;
+
+                var description = panelLeft.querySelector("." + this.className);
+                var value = panelRight.querySelector("." + this.className);
+
+                description.innerText = this.innerText;
+                value.innerText = this.dataset.price;
+
+                productPrice = parseInt(this.dataset.price);
+
+                summary();
+                list.classList.add("list_hide");
+            });
+        }
+    });
+
+    arrowColor.addEventListener("click", function (event) {
+        //Rozwijanie listy
+        var dropDown = this.parentElement;
+        var list = dropDown.querySelector(".list_panel");
+        list.classList.toggle("list_hide");
+
+        var choice = dropDown.querySelectorAll("li");
+        for (var i = 0; i < choice.length; i++) {
+
+            choice[i].addEventListener("click", function select(event) {
+
+                var dropDown = this.parentElement.parentElement;
+                var listLabel = dropDown.querySelector(".list_label");
+                listLabel.innerText = this.innerText;
+
+                var description = panelLeft.querySelector("." + this.className);
+                var value = panelRight.querySelector("." + this.className);
+
+                description.innerText = this.innerText;
+                value.innerText = this.dataset.price;
+
+                colorPrice = parseInt(this.dataset.price);
+
+                summary();
+                list.classList.add("list_hide");
+            });
+        }
+    });
+
+
+    arrowPattern.addEventListener("click", function (event) {
+        //Rozwijanie listy
+        var dropDown = this.parentElement;
+        var list = dropDown.querySelector(".list_panel");
+        list.classList.toggle("list_hide");
+
+        var choice = dropDown.querySelectorAll("li");
+        for (var i = 0; i < choice.length; i++) {
+
+            choice[i].addEventListener("click", function select(event) {
+
+                var dropDown = this.parentElement.parentElement;
+                var listLabel = dropDown.querySelector(".list_label");
+                listLabel.innerText = this.innerText;
+
+                var description = panelLeft.querySelector("." + this.className);
+                var value = panelRight.querySelector("." + this.className);
+
+                description.innerText = this.innerText;
+                value.innerText = this.dataset.price;
+
+                patternPrice = parseInt(this.dataset.price);
+
+                summary();
+                list.classList.add("list_hide");
+            });
+        }
+    });
+
+
+    //TRANSPORT
+    var transportBox = document.getElementById("transport");
+    var transportTitle = panelLeft.querySelector(".transport");
+    var transportValue = panelRight.querySelector(".transport");
+
+    transportBox.addEventListener('click', function() {
+        if (transportBox.checked) {
+            transportTitle.innerHTML = "Transport";
+            transportValue.innerHTML = transportBox.dataset.transportPrice;
+            transportPrice = parseInt(transportBox.dataset.transportPrice);
+            summary();
+        } else {
+            transport.innerHTML = "";
+            transportValue.innerHTML = "0";
+            transportPrice = 0;
+            summary();
+        }
+    });
+
 });
+
+//
+// //CALCULATOR
+// var arrows = document.querySelectorAll(".list_arrow");
+// var panelLeft = document.querySelector(".panel_left");
+// var panelRight = document.querySelector(".panel_right");
+// var productPrice = panelRight.querySelector(".product");
+// var colorPrice = panelRight.querySelector(".color");
+// var patternPrice = panelRight.querySelector(".pattern");
+// var totalPrice = document.querySelector(".sum_total");
+//
+// // var productPrice = 0;
+// // var colorPrice = 0;
+// // var fabricPrice = 0;
+// // var transportPrice = 0;
+// // var summaryPrice = 0;
+//
+//
+// for (var j = 0 ; j < arrows.length; j++){
+//     arrows[j].addEventListener("click", show);
+// }
+//
+// function show(event) {
+//     //Rozwijanie listy
+//     var dropDown = this.parentElement;
+//     var list = dropDown.querySelector(".list_panel");
+//     list.classList.toggle("list_hide");
+//
+//
+//     //Wybieranie poszczególnych elementów
+//     var choice = dropDown.querySelectorAll("li");
+//     for (var i = 0 ; i < choice.length; i++){
+//         choice[i].addEventListener("click", select)
+//     }
+//
+//     function select(event){
+//         var dropDown = this.parentElement.parentElement;
+//         var listLabel =  dropDown.querySelector(".list_label");
+//         listLabel.innerText = this.innerText;
+//
+//         var description = panelLeft.querySelector("."+this.className);
+//         var value = panelRight.querySelector("."+this.className);
+//
+//         description.innerText = this.innerText;
+//         value.innerText = this.dataset.price;
+//
+//         var price = parseInt(this.dataset.price);
+//         summary();
+//         list.classList.add("list_hide");
+//     }
+// }
+//
+// //TRANSPORT
+// var transportBox = document.getElementById("transport");
+// var transportTitle = panelLeft.querySelector(".transport");
+// var transportPrice = panelRight.querySelector(".transport");
+//
+// transportBox.addEventListener('click', function() {
+//     if (transportBox.checked) {
+//         transportTitle.innerHTML = "Transport";
+//         transportPrice.innerHTML = transportBox.dataset.transportPrice;
+//         summary();
+//     } else {
+//         transport.innerHTML = "";
+//         transportPrice.innerHTML = "0";
+//     }
+// });
+//
+// function summary(){
+//     summaryPrice = parseInt(productPrice.innerText) + parseInt(colorPrice.innerText) + parseInt(patternPrice.innerText) +  parseInt(transportPrice.innerText);
+//     totalPrice.innerText = summaryPrice;
+// }
+
+
 
